ac2471f4b3db59c5aa5891105ce174e26f7cc5d6
dimasmikaeltag <dimas@tag2u.com.br>
2022-10-01 17:24:03 -0300

    add splashscreen

--- lib/app/ui/android/pages/initial/initial_page.dart
+++ lib/app/ui/android/pages/initial/initial_page.dart
@@ -1,10 +1,32 @@
+import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:konsi_app/app/ui/android/pages/login/login_page.dart';
+import 'package:page_transition/page_transition.dart' show PageTransitionType;
 
 class InitialPage extends StatelessWidget {
   const InitialPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: Text("kkk"),),);
+    return Stack(children: [
+      AnimatedSplashScreen(
+        splash: Container(
+         // color: Colors.blue,
+          width: 200,
+          child: Image.asset(
+            'assets/images/logo-konsi.png',
+            fit: BoxFit.contain,
+          ),
+        ),
+
+        nextScreen: const LoginPage(),
+        // loggedUser == null ? const LoginView() : const HomeView(),
+        splashTransition: SplashTransition.scaleTransition,
+        pageTransitionType: PageTransitionType.leftToRight,
+        backgroundColor: Colors.white,
+        duration: 5000,
+        splashIconSize: 300,
+      ),
+    ]);
   }
 }
