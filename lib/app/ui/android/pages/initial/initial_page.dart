5f7660432fc8bc866f2472a0c36d4dcdd358d5b2
dimasmikaeltag <dimas@tag2u.com.br>
2022-10-01 17:37:46 -0300

    add sizeconfig

--- lib/app/ui/android/pages/initial/initial_page.dart
+++ lib/app/ui/android/pages/initial/initial_page.dart
@@ -1,5 +1,6 @@
 import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:konsi_app/app/ui/android/global_widgets/widget_size_configuration/size_config.dart';
 import 'package:konsi_app/app/ui/android/pages/login/login_page.dart';
 import 'package:page_transition/page_transition.dart' show PageTransitionType;
 
@@ -8,25 +9,23 @@ class InitialPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Stack(children: [
-      AnimatedSplashScreen(
-        splash: Container(
-         // color: Colors.blue,
-          width: 200,
-          child: Image.asset(
+    WidgetSizeConfig().init(context);
+    return Stack(
+      children: [
+        AnimatedSplashScreen(
+          splash: Image.asset(
             'assets/images/logo-konsi.png',
             fit: BoxFit.contain,
+            width: WidgetSizeConfig.safeBlockHorizontal! * 60,
           ),
+          nextScreen: const LoginPage(),
+          splashTransition: SplashTransition.scaleTransition,
+          pageTransitionType: PageTransitionType.leftToRight,
+          backgroundColor: Colors.white,
+          duration: 5000,
+          splashIconSize: 300,
         ),
-
-        nextScreen: const LoginPage(),
-        // loggedUser == null ? const LoginView() : const HomeView(),
-        splashTransition: SplashTransition.scaleTransition,
-        pageTransitionType: PageTransitionType.leftToRight,
-        backgroundColor: Colors.white,
-        duration: 5000,
-        splashIconSize: 300,
-      ),
-    ]);
+      ],
+    );
   }
 }
